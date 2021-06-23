258361ba887a97e6415b1b8e257c98eeddbd6fbd
Paul DeVito <pdevito3@gmail.com>
2021-06-22 22:35:50 -0400

    c#10 refactor comment

--- Ordering/src/Ordering.Core/Entities/Patient.cs
+++ Ordering/src/Ordering.Core/Entities/Patient.cs
@@ -43,6 +43,18 @@ namespace Ordering.Core.Entities
                 else
                     _sex = "Unknown";
             }
+
+            //init c#10, can change to this since i'm using the same data type
+            //get;
+            //set
+            //{
+            //    if (value.Equals("Male", StringComparison.InvariantCultureIgnoreCase) || value.Equals("M", StringComparison.InvariantCultureIgnoreCase))
+            //        field = "Male";
+            //    else if (value.Equals("Female", StringComparison.InvariantCultureIgnoreCase) || value.Equals("F", StringComparison.InvariantCultureIgnoreCase))
+            //        field = "Female";
+            //    else
+            //        field = "Unknown";
+            //}
         }
 
         [Sieve(CanFilter = true, CanSort = true)]
