6b4d3875212c804347350abcf40aeadb065b2000
Paul DeVito <pdevito3@gmail.com>
2021-06-27 11:00:34 -0400

    feat: absolute paths added

--- oncolims-bff/ClientApp/craco.config.js
+++ oncolims-bff/ClientApp/craco.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   style: {
     postcss: {
@@ -5,6 +7,11 @@ module.exports = {
         require('tailwindcss'),
         require('autoprefixer'),
       ],
+    },
+  },
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 }
